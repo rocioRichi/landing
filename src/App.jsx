90983485425ca128c61669ae1cdb339207ae391d
Rocio Ricciardiello <paragit@gmail.com>
2025-05-22 14:01:20 +0200

    Test add thepink logo

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,22 @@ import "./App.css";
 function App() {
   return (
     <>
+      <img src="tpdlanding.png" className="logo react" alt="logo" />
+      <div></div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-          <img src="laravellogo.png" className="logo react" alt="React logo" />
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo react" alt="Vite logo" />
+          </a>
+          <img
+            src="laravellogo.png"
+            className="logo react"
+            alt="Laravel logo"
+          />
         </a>
       </div>
-      <h1>Portfolio</h1>
+      <h3>Portfolio</h3>
 
       <p className="read-the-docs">Coming Soon!</p>
     </>
